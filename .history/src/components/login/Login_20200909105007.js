cc2e57e5e361dbe14b138a023281b797a2e395f2
sadegh <akbarisadegh382@gmail.com>
2020-09-09 11:53:42 +0430

    app updated

---
+++ .history/src/components/login/Login_20200909105007.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import logo from './img/logo.png'
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+ 
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    margin:'50px'
+  
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  textInput:{
+    width:'280px',
+    height:'40px',
+    backgroundColor :'#ECEFF1',
+    
+    padding:'8px'
+  
+},
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+       
+          <img src={logo} alt="logo"/>
+       
+        
+        <form className={classes.form} noValidate>
+          <TextField
+            className={classes.te}
+            margin="normal"
+            required
+            fullWidth
+            
+            label="شماره موبایل یا ایمیل"
+           
+          />
+        
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            ورود
+          </Button>
+         
+        </form>
+      </div>
+      
+    </Container>
+  );
+}
